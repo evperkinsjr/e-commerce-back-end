e0a79a6a76efe1c8ca3ff89ae7426f7c1e975fee
Enrico Perkins Jr <evperkinsjr@gmail.com>
2021-07-28 18:27:07 -0400

    fixed error in a foreignKey

--- models/index.js
+++ models/index.js
@@ -18,7 +18,7 @@ Category.hasMany(Product, {
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
-  foreignKey: 'project_id',
+  foreignKey: 'product_id',
 });
 
 // Tags belongToMany Products (through ProductTag)
